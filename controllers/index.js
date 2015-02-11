510c9fca8d7c282daa55644f4d08ee667e9516c8
Greg <wgmuecke@gmail.com>
2015-02-11 16:07:17 +0000

    add events to homepage

--- controllers/index.js
+++ controllers/index.js
@@ -2,15 +2,23 @@
 
 var events = require('../models/events');
 
+
 /**
  * Controller that renders our index (home) page.
  */
 function index (request, response) {
+  var curDateTime = new Date();
   var contextData = {
     'title': 'MGT 656',
     'tagline': 'You are doomed (just kidding).',
-    'events': events.all
+    'events': []
   };
+  for (var n=events.all.length-1; n >= 0; n--) {
+    var event = events.all[n];
+    if(event.date > curDateTime) {
+      contextData.events.push(event);
+    }
+  }
   response.render('index.html', contextData);
 }
 
